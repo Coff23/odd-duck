a88b5bc71760231dbf8efe8699f535682e453130
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-03-22 18:25:28 -0500

    does not work do not copy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const showedImages = [];
+let showedImages = [];
 let roundsOFVoting = 25;
 let results = document.getElementById("results");
 
@@ -12,25 +12,29 @@ function Choice(name, source) {
     this.source = source;
 }
 
-showedImages.push(new Choice("bag", "img/bag.jpg"));
-showedImages.push(new Choice("banana", "img/banana.jpg"));
-showedImages.push(new Choice("bathroom", "img/bathroom.jpg"));
-showedImages.push(new Choice("boots", "img/boots.jpg"));
-showedImages.push(new Choice("breakfast", "img/breakfast.jpg"));
-showedImages.push(new Choice("bubblegum", "img/bubblegum.jpg"));
-showedImages.push(new Choice("chair", "img/chair.jpg"));
-showedImages.push(new Choice("cthulhu", "img/cthulhu.jpg"));
-showedImages.push(new Choice("dog-duck", "img/dog-duck.jpg"));
-showedImages.push(new Choice("dragon", "img/dragon.jpg"));
-showedImages.push(new Choice("pen", "img/pen.jpg"));
-showedImages.push(new Choice("pet-sweep", "img/pet-sweep.jpg"));
-showedImages.push(new Choice("scissors", "img/scissors.jpg"));
-showedImages.push(new Choice("shark", "img/shark.jpg"));
-showedImages.push(new Choice("sweep", "img/sweep.png"));
-showedImages.push(new Choice("tauntaun", "img/tauntaun.jpg"));
-showedImages.push(new Choice("unicorn", "img/unicorn.jpg"));
-showedImages.push(new Choice("water-can", "img/water-can.jpg"));
-showedImages.push(new Choice("wine-glass", "img/wine-glass.jpg"));
+if (localStorage.getItem('showedImages')) {
+    showedImages = JSON.parse(localStorage.getItem(showedImages));
+} else {
+    showedImages.push(new Choice("bag", "img/bag.jpg"));
+    showedImages.push(new Choice("banana", "img/banana.jpg"));
+    showedImages.push(new Choice("bathroom", "img/bathroom.jpg"));
+    showedImages.push(new Choice("boots", "img/boots.jpg"));
+    showedImages.push(new Choice("breakfast", "img/breakfast.jpg"));
+    showedImages.push(new Choice("bubblegum", "img/bubblegum.jpg"));
+    showedImages.push(new Choice("chair", "img/chair.jpg"));
+    showedImages.push(new Choice("cthulhu", "img/cthulhu.jpg"));
+    showedImages.push(new Choice("dog-duck", "img/dog-duck.jpg"));
+    showedImages.push(new Choice("dragon", "img/dragon.jpg"));
+    showedImages.push(new Choice("pen", "img/pen.jpg"));
+    showedImages.push(new Choice("pet-sweep", "img/pet-sweep.jpg"));
+    showedImages.push(new Choice("scissors", "img/scissors.jpg"));
+    showedImages.push(new Choice("shark", "img/shark.jpg"));
+    showedImages.push(new Choice("sweep", "img/sweep.png"));
+    showedImages.push(new Choice("tauntaun", "img/tauntaun.jpg"));
+    showedImages.push(new Choice("unicorn", "img/unicorn.jpg"));
+    showedImages.push(new Choice("water-can", "img/water-can.jpg"));
+    showedImages.push(new Choice("wine-glass", "img/wine-glass.jpg"));
+}
 
 // Location where the images are displaying on the screen
 let imageEl1 = document.querySelector("section img:first-child");
@@ -54,21 +58,21 @@ function renderImages() {
         image1 = showedImages[generateRandomImage()];
         image2 = showedImages[generateRandomImage()];
     }
-    
+
     imageEl1.src = image1.source;
     imageEl1.id = image1.name;
     image1.timesShown += 1;
-    
+
     imageEl2.src = image2.source;
     imageEl2.id = image2.name;
     image2.timesShown += 1;
-    
+
     imageEl3.src = image3.source;
     imageEl3.id = image3.name;
     image3.timesShown += 1;
-    
+
     // let previousImage = [image1, image2, image3];
-    
+
     // while (previousImage === imageEl1 || previousImage === imageEl2 || previousImage === imageEl3) {
     //     imageEl1 = showedImages[generateRandomImage()];
     //     imageEl1 = showedImages[generateRandomImage()];
@@ -146,20 +150,28 @@ function renderData(event) {
         }
     });
 }
+
 renderImages();
 
-localStorage.setItem("Images", JSON.stringify(showedImages));
-console.log(localStorage);
-let choiceState = localStorage.getItem("Images");
-console.log(JSON.parse(choiceState));
+// localStorage.setItem("Choice", JSON.stringify(showedImages));
+// console.log(localStorage);
+// let choiceState = localStorage.getItem('Choice');
+// console.log(JSON.parse(choiceState));
 
 function writeData(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
+    localStorage.setItem('Choice', JSON.stringify(showedImages));
+    console.log(localStorage);
 }
+
+writeData();
 
 function readData(key) {
-    return JSON.parse(localStorage.getItem(key));
+    return JSON.parse(localStorage.getItem('Choice')) || [];
+    // console.log(JSON.parse('Choice'));
 }
 
-writeData("choice", showedImages);
-console.log(readData("Images"));
+readData();
+
+
+// writeData();
+// readData();
